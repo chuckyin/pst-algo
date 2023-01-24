ecef3ddb4b4bc60e2970e439e06cdc528b50eb3d
melshaer0612 <melshaer0612@meta.com>
2023-01-24 10:30:17 -0800

    Integrated jmp script into process_csv(). Included suggestions for plots (scatter, contour, hexbins, density plots)

--- blob_detection_mc.py
+++ blob_detection_mc.py
@@ -12,13 +12,14 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-import time
-from datetime import timedelta
-
-from skimage.transform import radon
-
+import matplotlib.cm as cm
+import seaborn as sns
 import re
+import time
+
+from datetime import timedelta
+from skimage.transform import radon
+from scipy.interpolate import griddata
 
 
 class Frame:
@@ -213,31 +214,32 @@ class Dot:
     
 current_path = os.getcwd()
 
-input_path = os.path.join(current_path, '60deg_test_55p5' + '/')
-output_path = os.path.join(current_path, '60deg_test_55p5_output' + '/')
-image_files = glob.glob(input_path + '*.tiff')
+# input_path = os.path.join(current_path, '60deg_test_55p5' + '/')
+# output_path = os.path.join(current_path, '60deg_test_55p5_output' + '/')
+# image_files = glob.glob(input_path + '*.tiff')
 
-# image_files = ['test2.tiff']
-# input_path = current_path
-# output_path = current_path
+image_files = ['test2.tiff']
+input_path = current_path
+output_path = current_path
 
 #----Xuan test------
 
-current_path = r'/Users/xuawang/Dropbox (Meta)/Pupil Swim Metrology/PST/20221121 2d dots 60deg/Arcata EVT2 A/test_55p5/'
+# current_path = r'/Users/xuawang/Dropbox (Meta)/Pupil Swim Metrology/PST/20221121 2d dots 60deg/Arcata EVT2 A/test_55p5/'
 
-input_path = current_path
-output_path = os.path.join(current_path, 'output' + '/')
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+# input_path = current_path
+# output_path = os.path.join(current_path, 'output' + '/')
+# if not os.path.exists(output_path):
+#     os.makedirs(output_path)
 
-image_files = glob.glob(input_path + '*.tiff')
-image_files.sort(key=lambda f: int(re.sub('\D', '', f)))
-image_files =image_files[:1]
-#image_files =image_files[:110:10]  #every nth image
+# image_files = glob.glob(input_path + '*.tiff')
+# image_files.sort(key=lambda f: int(re.sub('\D', '', f)))
+# image_files =image_files[:1]
+# #image_files =image_files[:110:10]  #every nth image
 
 #-------------
 
 log_file = 'Detection Log_' + time.strftime('%Y%m%d-%H%M%S') + '.txt'
+csv_file = os.path.join(output_path, time.strftime('%Y%m%d-%H%M%S') + '_indexed_dots.csv')
 
 
 # dot_params = {'min_dist' : 3,
@@ -407,6 +409,104 @@ def draw_dots(image, dots, filename):
             cv2.circle(image_cpy, (int(dot.x), int(dot.y)), int(dot.size/2), (0, 0, 255), 3) # Red Circle    
     cv2.imwrite(os.path.join(output_path, filename), image_cpy)
     
+
+def process_csv(filename):
+    df = pd.read_csv(filename)
+    df_sub = df[['x', 'y', 'xi', 'yi', 'frame_num']]
+    df_sub = df_sub.rename(columns={'x':'x_temp', 'y':'y_temp'})
+
+    df['xi1'] = df['xi'] - 1
+    df['yi1'] = df['yi'] - 1
+
+    xi = df_sub['xi'].values
+    yi = df_sub['yi'].values
+    frame_num = df_sub['frame_num'].values
+    v = np.vstack([xi, yi, frame_num]).T.tolist()
+    vt = [tuple(vi) for vi in v]
+    df_sub['key'] = vt
+
+    xi1 = df['xi1'].values
+    yi1 = df['yi1'].values
+    xi_df = df['xi'].values
+    yi_df = df['yi'].values
+    frame_num_df = df['frame_num'].values
+    vx_df = np.vstack([xi1, yi_df, frame_num_df]).T.tolist()
+    vy_df = np.vstack([xi_df, yi1, frame_num_df]).T.tolist()
+    vxt_df = [tuple(vi) for vi in vx_df]
+    vyt_df = [tuple(vi) for vi in vy_df]
+    df['keyx'] = vxt_df
+    df['keyy'] = vyt_df
+
+    merged_df_x = pd.merge(df, df_sub[['x_temp', 'key']], left_on=['keyx'], right_on=['key'], how='left').drop(columns=['keyx', 'key'])
+    merged_df = pd.merge(merged_df_x, df_sub[['y_temp', 'key']], left_on=['keyy'], right_on=['key'], how='left').drop(columns=['keyy', 'key'])
+
+    merged_df['dx'] = merged_df['x'] - merged_df['x_temp']
+    merged_df['dy'] = merged_df['y'] - merged_df['y_temp']
+
+    df2 = pd.DataFrame()
+    df2['dx_med'] = merged_df.groupby(['xi', 'yi'])['dx'].median()
+    df2['dy_med'] = merged_df.groupby(['xi', 'yi'])['dy'].median()
+
+    df2.reset_index(inplace=True)
+
+    v_df2 = np.vstack([df2['xi'].values, df2['yi'].values]).T.tolist()
+    v_merged = np.vstack([merged_df['xi'].values, merged_df['yi'].values]).T.tolist()
+    vt_df2 = [tuple(vi) for vi in v_df2]
+    vt_merged = [tuple(vi) for vi in v_merged]
+    df2['key'] = vt_df2
+    merged_df['key'] = vt_merged
+
+    merged_df2 = pd.merge(merged_df, df2[['dx_med', 'dy_med', 'key']], on=['key'], how='left').drop(columns=['key'])
+
+    merged_df2['dx_norm'] = merged_df2['dx'] / merged_df2['dx_med']
+    merged_df2['dy_norm'] = merged_df2['dy'] / merged_df2['dy_med']
+    
+    # ------------------------------?
+    # Plots
+    y = merged_df2['yi'].tolist()
+    x = merged_df2['xi'].tolist()
+    dx = merged_df2['dx_norm'].tolist()
+    dy = merged_df2['dy_norm'].tolist()
+    
+    # Suggestion 1: Hexbin Plots
+    plt.hexbin(x, y, dx, cmap=cm.jet)
+    plt.colorbar()
+    plt.title('dx')
+    plt.figure()
+    plt.hexbin(x, y, dy, cmap=cm.jet)
+    plt.colorbar()
+    plt.title('dy')
+
+    # Suggestion 2: Scatter Plots
+    plt.scatter(x, y, c=dx, cmap=cm.jet)
+    plt.colorbar()
+    plt.figure()
+    plt.scatter(x, y, c=dy, cmap=cm.jet)
+    plt.colorbar()
+    
+    # Suggestion 3: Joint Distribution Plots
+    sns.jointplot(x = merged_df2['xi'], y = merged_df2['yi'], kind = "hex", data = merged_df2['dx'])
+    sns.jointplot(x = merged_df2['xi'], y = merged_df2['yi'], kind = "kde", data = merged_df2['dx'])
+    sns.jointplot(x = merged_df2['xi'], y = merged_df2['yi'], kind = "scatter", data = merged_df2['dx'])
+    
+    # Suggestion 4: Contour Plots
+    data = np.array([x, y, dx]).T
+    X, Y = np.meshgrid(data[:,0], data[:,1])
+    Z = griddata((data[:,0], data[:,1]), data[:,2], (X, Y), method='nearest')
+    plt.contourf(X, Y, Z)
+    
+    # Or
+    levels = 0.5
+    plt.contour(X, Y, Z, levels=levels)
+    
+    # Or
+    merged_na = merged_df2.dropna()
+    y = merged_na['yi'].tolist()
+    x = merged_na['xi'].tolist()
+    dx = merged_na['dx_norm'].tolist()
+    dy = merged_na['dy_norm'].tolist()
+    plt.tricontourf(x, y, dx)
+    
     
 if __name__ == '__main__':
     start_time = time.monotonic()
@@ -476,9 +576,11 @@ if __name__ == '__main__':
         df = pd.concat([df, mini_df])
         print('Total frames processed is', str(cnt), '/ 150')
         
-    df.to_csv(os.path.join(output_path,'indexed_dots.csv'))
+    df.to_csv(csv_file)
     end_time = time.monotonic()
     print(timedelta(seconds=end_time - start_time))
+    
+    process_csv(csv_file)
     
 
         
