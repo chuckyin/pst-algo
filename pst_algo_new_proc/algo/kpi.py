bfbf9be2c4be898e9886c223a1e8b5c7c93d4343
melshaer0612 <melshaer0612@meta.com>
2023-05-12 16:20:01 -0700

    Included new procedure algo code

---
+++ pst_algo_new_proc/algo/kpi.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: melshaer0612@meta.com
+
+"""
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import config.config as cf
+
+from config.logging import logger
+
+
+sys.dont_write_bytecode = True # Disables __pycache__
+
+
+def calc_median_map(maps, min_num=3):
+    #calculate the median map of a maps (list), with min number of valid elements (will output nan if <min_num)
+    cnt = 0
+    maps_arr = np.asarray(maps)
+    l, xi, yi, dim = np.shape(maps_arr)
+    output = np.empty((xi, yi, dim))
+    if min_num > l:
+        min_num = l
+        logger.warning('Warning: min number of valid elements reduce to: %d', l)
+    
+    for i in range(xi):
+        for j in range(yi):
+            for k in range(dim):
+                if np.count_nonzero(~np.isnan(maps_arr[:, i, j, k])) >= min_num:
+                    output[i, j, k] = np.nanmedian(maps_arr[:, i, j, k])
+                    cnt += 1
+                else:
+                    output[i,j,k] = np.nan
+    logger.info('Median map calculation complete with %d dots' , cnt)                
+    return output
+
+
+def plot_map_norm(map_norm, fname_str=''):
+    xi_full, yi_full, dim = map_norm.shape
+    xi_range = int((xi_full - 1) / 2)
+    yi_range = int((yi_full - 1) / 2)
+    X, Y = np.meshgrid(np.linspace(-xi_range,xi_range,xi_full),np.linspace(-yi_range,yi_range,yi_full))
+    
+    for i in [0, 1]: #x,y
+        if i == 0:
+            axis = 'X'
+        else:
+            axis = 'Y'
+        fig = plt.figure(figsize=(5, 5), dpi=200) 
+        ax = fig.add_subplot(111)
+        plt.title('Normalized ' + axis + '-Spacing Change')
+        plt.xlabel('xi')
+        plt.ylabel('yi')
+        levels = np.linspace(0.95, 1.05, 11)
+        plt.contourf(X, Y, map_norm[:,:,i].T, levels=levels, cmap='seismic', extend='both')  #cmap ='coolwarm','bwr'
+        ax.set_aspect('equal')
+    
+        plt.xlim(-xi_range, xi_range)
+        plt.ylim(yi_range, -yi_range)
+        
+        radii = [25, 45]
+    
+        for radius in radii:    
+            theta = np.linspace(0, 2 * np.pi, 100)
+            a = radius * np.cos(theta)
+            b = radius * np.sin(theta)
+            plt.plot(a, b, color='green', linestyle=(0, (5, 5)), linewidth=0.4)  #'loosely dotted'
+            
+        plt.grid(color='grey', linestyle='dashed', linewidth=0.2)
+        plt.rcParams['font.size'] = '5'
+        plt.colorbar()
+        plt.savefig(os.path.join(cf.output_path, 'Normalized_Map_' + fname_str + axis +'.png'))
+
+    
+def plot_map_global(map_global, fname_str=''):   
+    xi_full, yi_full = map_global.shape
+    xi_range = int((xi_full -1) / 2)
+    yi_range = int((yi_full -1) / 2)
+    X, Y = np.meshgrid(np.linspace(-xi_range, xi_range, xi_full), np.linspace(-yi_range, yi_range, yi_full))
+    #map_global = np.ma.array(map_global, mask=np.isnan(map_global))
+
+    fig = plt.figure(figsize=(5, 5), dpi=200) 
+    ax = fig.add_subplot(111)
+    plt.title('Global PS Map ' + fname_str + ' [degree]')
+    plt.xlabel('xi')
+    plt.ylabel('yi')
+    levels = np.linspace(0, 1.0, 6)
+    plt.contourf(X, Y, map_global[:, :].T, levels=levels, cmap='coolwarm', extend ='max')  #cmap ='coolwarm','bwr'
+    ax.set_aspect('equal')
+
+    plt.xlim(-xi_range, xi_range)
+    plt.ylim(yi_range, -yi_range)
+    
+    radii = [25, 45]
+
+    for radius in radii:    
+        theta = np.linspace(0, 2 * np.pi, 100)
+        a = radius * np.cos(theta)
+        b = radius * np.sin(theta)
+        plt.plot(a, b, color='green', linestyle=(0, (5, 5)), linewidth=0.4)  #'loosely dotted'
+        
+    plt.grid(color='grey', linestyle='dashed', linewidth=0.2)
+    plt.rcParams['font.size'] = '5'
+    plt.colorbar()
+    plt.savefig(os.path.join(cf.output_path, 'Global_PS_Map_' + fname_str + '.png'))       
+
+    
+def offset_map_fov (map_input, xi_fov, yi_fov):
+    def shift_fillnan (arr, shift, axis):
+        arr = np.roll(arr, shift, axis)
+        if axis == 0:
+            if shift >= 0:
+                arr[:shift, :] = np.nan
+            else:
+                arr[shift:, :] = np.nan
+        elif axis == 1:
+            if shift >= 0:
+                arr[:, :shift] = np.nan
+            else:    
+                arr[:, shift:] = np.nan
+        
+        return arr    
+    
+    #offset map and create FOV map (center of display)
+    map_output = shift_fillnan(map_input, int(np.round(-xi_fov)), axis=0) #shift map relative to FOV 
+    map_output = shift_fillnan(map_output, int(np.round(-yi_fov)), axis=1)
+    
+    return map_output
+
+    
+def calc_parametrics_local(map_local, map_fov):
+    #calculate the parametrics based on the map
+    summary = {}
+    start_r = -1
+    radii = [25, 35, 45, 60]  #need to start from small to large
+    axis = ['x', 'y']
+    th = [0.5, 1, 5]  #in percent
+    
+    #define zones
+    for i in range(len(radii)):
+        zone = (map_fov > start_r) * (map_fov <= radii[i])
+        start_r = radii[i]        
+        mapp_both_x = []
+        for j in range(len(axis)):            
+            mapp = map_local[:, :, j]
+            summary['local_areatotal_d' + axis[j] + '_' + str(radii[i])] = np.count_nonzero(~np.isnan(mapp[zone]))
+            summary['local_max_d' + axis[j] + '_' + str(radii[i])] = (np.nanmax(mapp[zone]) - 1) * 100
+            summary['local_min_d' + axis[j] + '_' + str(radii[i])] = (np.nanmin(mapp[zone]) - 1) * 100
+            summary['local_pct99_d' + axis[j] + '_' + str(radii[i])] = (np.nanpercentile(mapp[zone], 99) - 1) * 100
+            summary['local_pct1_d' + axis[j] + '_'+ str(radii[i])] = (np.nanpercentile(mapp[zone], 1) - 1) * 100
+            summary['local_rms_d' + axis[j] + '_' + str(radii[i])] = (np.nanstd(mapp[zone])) * 100
+            for k in range(len(th)): 
+                mapp_pos = (mapp >= 1 + th[k] / 100) * zone
+                mapp_neg = (mapp <= 1 - th[k] / 100) * zone
+                mapp_both = ((mapp >= 1 + th[k]/100) + (mapp <= 1 - th[k] / 100)) * zone
+                summary['local_area_d' + axis[j] + '_th'+ str(th[k]) + 'pctpos_' + str(radii[i])] = np.count_nonzero(mapp_pos)
+                summary['local_area_d' + axis[j] + '_th'+ str(th[k]) + 'pctneg_' + str(radii[i])] = np.count_nonzero(mapp_neg)
+                summary['local_area_d' + axis[j] + '_th' + str(th[k]) + 'pct_' + str(radii[i])] = np.count_nonzero(mapp_both)
+                if j == 0: # x maps
+                    mapp_both_x.append(mapp_both)
+                elif j == 1: # y maps
+                    mapp_both_combine = mapp_both + mapp_both_x[k]
+                    summary['local_area_combined_th' + str(th[k]) + 'pct_' + str(radii[i])] = np.count_nonzero(mapp_both_combine)
+                
+    return summary
+
+
+def calc_parametrics_global(map_global, map_fov, label, frame_num):
+    #calculate the parametrics based on the map    
+    summary = {}
+    start_r = -1
+    radii = [25, 35, 45, 60]  #need to start from small to large
+    summary['global_' + label + '_frame_num'] = frame_num
+    
+    #define zones
+    for i in range(len(radii)):
+        zone =  (map_fov > start_r) * (map_fov <= radii[i])
+        start_r = radii[i]
+        summary['global_' + label + '_max_' + str(radii[i])] = np.nanmax(map_global[zone])
+        summary['global_' + label + '_pct99_' + str(radii[i])] = np.nanpercentile(map_global[zone], 99)
+        summary['global_' + label + '_median_' + str(radii[i])] = np.nanpercentile(map_global[zone], 50)
+
+    return summary
+
+
+def eval_KPIs(df_frame, params, summary_df, frame_nums, maps_xy, maps_dxdy, middle_dxdy):
+    def calc_distance(map_xy, ref0):
+        #calculate distance map, relative to ref0, ref0 could be map of same shape, or x,y coor 1-dim array
+        xi_full, yi_full, _ = map_xy.shape
+
+        map_distance = np.empty((xi_full, yi_full))
+        map_distance[:,:] = np.nan
+        
+        map_xy_delta = map_xy - ref0
+        
+        for i in range(xi_full):
+            for j in range(yi_full):
+                map_distance[i, j] = np.sqrt((map_xy_delta[i, j, 0] ** 2 + map_xy_delta[i, j, 1] ** 2))
+        
+        return map_distance
+
+    map_dxdy_median = calc_median_map(maps_dxdy, min_num=1)
+    assert(params['dxdy_spacing'] > 0)
+    unitspacing_xy = map_dxdy_median[60, 60] / params['dxdy_spacing']
+    xx, yy = np.meshgrid(np.linspace(-60, 60, 121), np.linspace(-60, 60, 121))
+    map_fov = np.sqrt(xx ** 2 + yy ** 2) + sys.float_info.epsilon # takes care of divide-by-zero warning
+    
+    summary = summary_df.to_dict(orient='records')[0]
+    (xi_fov, yi_fov) = [summary['fov_dot_x'] - summary['center_dot_x'], summary['fov_dot_y'] - summary['center_dot_y']] / unitspacing_xy
+    summary['version'] = cf.get_version()
+    summary['xi_fov'] = xi_fov
+    summary['yi_fov'] = yi_fov
+    logger.info('FOV dot is indexed at (%0.2f, %0.2f)', xi_fov, yi_fov)
+    
+    # Local PS
+    try:
+        map_dxdy_norm = middle_dxdy / map_dxdy_median # Normalize map_dxdy
+        map_dxdy_norm_fov = offset_map_fov(map_dxdy_norm, xi_fov, yi_fov)
+        
+        # Filter resultant map by removing filter_percent
+        axis = ['x', 'y']
+        filtered_map = np.empty(np.shape(map_dxdy_norm_fov))
+        filtered_map.fill(np.nan)
+        for j in range(len(axis)):
+            mapj = map_dxdy_norm_fov[:, :, j]
+            upper = 1 + (params['filter_percent'] / 100)
+            lower = 1 - (params['filter_percent'] / 100)
+            resj = np.where(((mapj > upper) | (mapj < lower)) & (upper > lower), np.nan, mapj)
+            filtered_map[:, :, j] = resj
+            # if j == 0:
+            #     df = pd.DataFrame(resj)
+            #     df.to_csv(os.path.join(cf.output_path,'map_norm_dx_filtered.csv'))
+            
+        summary_local = calc_parametrics_local(map_dxdy_norm_fov, map_fov)
+        summary.update(summary_local) 
+        plot_map_norm(filtered_map)
+        #plot_map_norm(map_dxdy_norm_fov)
+    except ValueError:
+        logger.error('Error calculating and plotting local PS map')
+        pass
+    
+    # # Global PS
+    # map_distance = calc_distance(maps_xy[middle_frame_index], maps_xy[middle_frame_index][60, 60, :])
+    # map_unit = map_distance / map_fov + sys.float_info.epsilon # takes care of divide-by-zero warning
+    # df_frame_no_outliers = df_frame[(df_frame['flag_center_dot_outlier'] == 0) & (df_frame['flag_fov_dot_outlier'] == 0) & (df_frame['flag_slope_outlier'] == 0)] # filter out outlier frames
+      
+    # for i in [0, -1]: #first and last frame
+    #     if i == 0:
+    #         label = 'Right Gaze'
+    #     else:
+    #         label = 'Left Gaze'
+    #     try:
+    #         idx = df_frame_no_outliers.index[i]
+    #         map_delta_global = maps_xy[idx] - maps_xy[middle_frame_index]
+    #         map_distance_global = calc_distance(map_delta_global, map_delta_global[60, 60, :])
+    #         map_global = map_distance_global / map_unit
+    #         map_global = offset_map_fov(map_global, xi_fov, yi_fov)
+    #         summary_global = calc_parametrics_global (map_global, map_fov, label, frame_nums)
+    #         summary.update(summary_global)
+    #         plot_map_global(map_global, fname_str=label)
+    #     except ValueError:
+    #         logger.error('Error calculating and plotting global PS map')
+    #         pass
+            
+    return summary
+           
+
+def find_outliers(df_frame, width, height):
+    df_frame.set_index('index', inplace=True)   # use the index column as the new df_frame index 
+    #determine center dot and FOV outliers
+    median_center_x = np.median(df_frame['center_dot_x'])  # median of center dot locations, use this to determine center dot outlier
+    median_center_y = np.median(df_frame['center_dot_y'])
+    if (median_center_x > 2/3 * width) or (median_center_x < 1/3 * width) or (median_center_y > 2/3 * height) or (median_center_y < 1/3 * height): 
+        raise Exception('Error: Center dot outside of ROI (middle 1/3)')
+        logger.exception('Error: Center dot outside of ROI (middle 1/3)')
+    df_frame['dist_center_dot'] = np.nan
+    df_frame['dist_fov_center'] = np.nan
+    df_frame['flag_center_dot_outlier'] = 0
+    df_frame['flag_fov_dot_outlier'] = 0
+    df_frame['flag_slope_outlier'] = 0
+    num_outliers = 0 
+    num_fov_outliers = 0
+    num_slope_outliers = 0
+    for i in range(len(df_frame.index)):
+        # determine if center dot is outlier based on distance to the median location, if > 50px, mark as outlier
+        df_frame.loc[i, 'dist_center_dot'] = np.sqrt((df_frame.loc[i,'center_dot_x'] - median_center_x) ** 2 + (df_frame.loc[i,'center_dot_y'] - median_center_y) ** 2)
+        if df_frame.loc[i,'dist_center_dot'] > 50:
+            df_frame.loc[i,'flag_center_dot_outlier']= 1
+            num_outliers += 1
+            logger.warning('Center dot outlier detected on frame #%s', df_frame.loc[i,'frame_num'])
+
+        # determine if FOV dot is outlier, if d < 25px, mark as outlier, if y distance > 200px, outlier   
+        df_frame.loc[i, 'dist_fov_center'] = np.sqrt((df_frame.loc[i,'fov_dot_x'] - df_frame.loc[i,'center_dot_x']) ** 2 + (df_frame.loc[i,'fov_dot_y'] - df_frame.loc[i,'center_dot_y']) ** 2)        
+        if (df_frame.loc[i,'dist_fov_center'] < 25) or (np.abs(df_frame.loc[i,'fov_dot_y'] - df_frame.loc[i,'center_dot_y'])) > 200:
+            df_frame.loc[i, 'flag_fov_dot_outlier'] = 1
+            num_fov_outliers += 1
+            logger.warning('FOV dot outlier detected on frame #%s', df_frame.loc[i,'frame_num'])
+        
+        # check slope    
+        if (np.abs(df_frame.loc[i,'hor_slope']) > 0.1) or (np.abs(df_frame.loc[i,'ver_slope']) > 0.1):
+            df_frame.loc[i, 'flag_slope_outlier'] = 1
+            num_slope_outliers +=1
+            logger.warning('Slope outlier detected on frame #%s', df_frame.loc[i,'frame_num'])
+
+    df_frame['num_frames'] = len(df_frame.index)
+    df_frame['num_center_dot_outlier'] = num_outliers
+    df_frame['num_fov_dot_outlier'] = num_fov_outliers
+    df_frame['num_slope_outlier'] = num_slope_outliers
+    df_frame['num_total_outlier'] = num_outliers + num_fov_outliers + num_slope_outliers
+    
---
+++ pst_algo_new_proc/pst-cli.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Pupil Swim Tester (PST) Algorithm Entry Point   -- Parallel Version. 
+
+Runs on multiple core CPUs for faster processing.        
+
+@author: melshaer0612@meta.com
+
+Usage:
+    pst-cli.py -h
+    pst-cli.py -d <dataset_name> [-p <parameter_file_name>]
+    
+Options:
+    -h              Display usage help message
+    -d --dataset    Supply name of dataset to be processed
+    -p --params     Supply JSON file containing parameters to be used for processing
+    
+    
+"""
+
+import time
+
+start_time = time.monotonic()
+
+import os
+import sys
+import getopt
+import glob
+import re
+import cv2
+import numpy as np
+import pandas as pd
+import csv
+import logging
+import multiprocessing as mp
+import algo.blobs as blobs
+import algo.kpi as kpi
+import config.config as cf
+
+from functools import partial
+from datetime import timedelta
+from logging.handlers import QueueHandler
+from config.logging import setup_logger, logger_process
+
+
+sys.dont_write_bytecode = True # Disables __pycache__
+
+
+def pipeline(queue, df_lst, df_frame_lst, frame_nums, maps_xy, maps_dxdy, output_path, params, image_file):
+    #------Logging------
+    logger = logging.getLogger(__name__)
+    if not logger.hasHandlers():
+        logger.addHandler(QueueHandler(queue))
+    logger.setLevel(logging.DEBUG)
+    
+    frame_num = ((image_file.split(os.path.sep)[-1].split('_'))[-1].split('.tiff'))[0]
+    frame_nums.append(frame_num)
+    
+    image = cv2.imread(image_file)
+    if params['driver'] == 'MODEL':
+        center_y_shift = params['model_center_y_shift']
+    elif params['driver'] == 'FATP': # rotate 180 deg as FATP is mounted upside down
+        center_y_shift = params['fatp_center_y_shift']
+        image = np.rot90(image, 2)  
+    
+    logger.info('Frame %s : %s Processing started', frame_num, params['driver'])
+    height, width, _ = image.shape
+    
+    fov_dot = blobs.find_fov(image, params, logger, frame_num, height, width)
+    logger.info('Frame %s : FOV dot was found at %s', frame_num, fov_dot.__str__())
+   
+    # Mask the detected FOV dot
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    mask = np.zeros_like(image_gray)
+    cv2.circle(mask, (int(fov_dot.x), int(fov_dot.y)), int(np.sqrt(fov_dot.size / np.pi) + 7), 255, -1)
+    image_gray = cv2.bitwise_and(image_gray, cv2.bitwise_not(mask))
+    cv2.imwrite(os.path.join(output_path, frame_num+'_no_fov.jpeg'), image_gray, [cv2.IMWRITE_JPEG_QUALITY, 40]) 
+   
+    frame = blobs.find_dots(image_gray, params)
+    logger.info('Frame %s : Finding dots is complete, found %d dots', frame_num, len(frame.dots))
+    
+    frame.center_dot = blobs.find_center_dot(frame.dots, height, width)
+    logger.info('Frame %s : Center dot was found at %s', frame_num, frame.center_dot.__str__())
+    
+    blobs.draw_dots(image, [fov_dot, frame.center_dot], os.path.join(output_path, frame_num+'_fov_center_dots.jpeg'))
+    blobs.draw_dots(image, frame.dots, os.path.join(output_path, frame_num+'_dots.jpeg')) # For debugging blob detection
+    
+    med_size, med_dist = frame.calc_dot_size_dist()
+    logger.info('Frame %s Dot Size: %0.2f Distance: %0.2f', frame_num, med_size, med_dist)
+    
+    logger.info('Starting slope calculations for frame %s', frame_num)
+    proc = blobs.prep_image(image, params, normalize_and_filter=True, binarize=False)
+    init_hor_slope, init_ver_slope, hor_dist_error, ver_dist_error = blobs.get_initial_slopes(proc, height, width, ratio=0.3)
+    hor_slope, ver_slope = frame.get_slopes(init_hor_slope, init_ver_slope, hor_dist_error, ver_dist_error)
+    logger.info('Frame %s HSlope: %0.2f VSlope: %0.2f', frame_num, hor_slope, ver_slope)
+    
+    hor_lines, ver_lines = frame.group_lines()
+    frame.draw_lines_on_image(image, width, height, filepath=os.path.join(output_path, frame_num+'_grouped.jpeg'))
+    frame.find_index(logger, frame_num, center_y_shift=center_y_shift)
+    logger.info('Finished indexing calculations for frame %s', frame_num)
+    
+    # generate maps
+    frame.generate_map_xy(logger, frame_num)
+    maps_xy[frame_num] = frame.map_xy
+    frame.generate_map_dxdy(params['dxdy_spacing'])  #4x spacing
+    maps_dxdy[frame_num] = frame.map_dxdy
+    
+    # Prepare to save results
+    x, y, xi, yi, size = [], [], [], [], []
+    xpts = [dot.x for dot in frame.dots]
+    ypts = [dot.y for dot in frame.dots]
+    sizepts = [dot.size for dot in frame.dots]
+    
+    for xpt, ypt, sizept in list(zip(xpts, ypts, sizepts)):
+        x.append(xpt)
+        y.append(ypt)
+        size.append(sizept)
+        if (xpt, ypt) in frame.dotsxy_indexed:
+            xi.append(frame.dotsxy_indexed[(xpt, ypt)][0][0])
+            yi.append(frame.dotsxy_indexed[(xpt, ypt)][0][1]) 
+        else:
+            xi.append(np.nan)
+            yi.append(np.nan) 
+    
+    # Write results to dataframe
+    mini_df_frame = pd.DataFrame({'frame_num': frame_num,
+                                  'total_dots' : len(frame.dots),
+                                  'center_dot_x' : frame.center_dot.x, 'center_dot_y' : frame.center_dot.y,
+                                  'fov_dot_x' : fov_dot.x, 'fov_dot_y' : fov_dot.y,
+                                  'median_dot_size' : med_size, 'median_dot_spacing' : med_dist,
+                                  'hor_slope' : hor_slope, 'ver_slope' : ver_slope,
+                                  'dxdy_spacing' : params['dxdy_spacing']}, index=[0])
+    
+    mini_df = pd.DataFrame({'frame_num' : frame_num, 'x' : x, 'y' : y, 'size' : size, 'xi' : xi, 'yi' : yi})
+    
+    df_frame_lst.append(mini_df_frame)
+    df_lst.append(mini_df)
+ 
+    
+if __name__ == '__main__':
+    current_path = os.getcwd()
+    dataset_folder = ''
+    params_file = ''
+    opts, args = getopt.getopt(sys.argv[1:],'hd:p:')
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('exe -d <dataset> -p <optional_parameter_file>')
+            sys.exit()
+        elif opt in ('-d', '--dataset'):
+            dataset_folder = arg
+        elif opt in ('-p', '--params'):
+            params_file = arg
+        
+    print ('Dataset: ', os.path.join(current_path, 'data', dataset_folder))
+    print ('Parameters File: ', os.path.join(current_path, 'config', params_file))
+    
+    params = cf.config(dataset_folder, params_file)
+    
+    log_file = os.path.join(cf.output_path, 'Log_' + time.strftime('%Y%m%d-%H%M%S') + '.log')
+    csv_file = os.path.join(cf.output_path, time.strftime('%Y%m%d-%H%M%S') + 'dots.csv')
+    csv_file_frame = os.path.join(cf.output_path, time.strftime('%Y%m%d-%H%M%S') + 'frames.csv')
+    csv_file_summary = os.path.join(cf.output_path, time.strftime('%Y%m%d-%H%M%S') + 'summary.csv')
+    
+    logger = setup_logger(filename=log_file)
+        
+    image_files_all = glob.glob(cf.input_path + '*.tiff')
+    image_files_all.sort(key=lambda f: int(re.sub('\D', '', f)))
+    
+    if params['num_frames'] == 10:
+        frame_num_list = ['15', '60', '70', '75', '80', '85', '90', '95', '105', '150']
+        image_files = [image_file for image_file in image_files_all 
+                      if ((image_file.split(os.path.sep)[-1].split('_'))[-1].split('.tiff'))[0] in frame_num_list]
+    elif params['num_frames'] == 16:
+        frame_num_list = ['15', '30', '45', '60', '65', '70', '75', '80', '85', '90', '95', '100', '105', '120', '135', '150']
+        image_files = [image_file for image_file in image_files_all 
+                      if ((image_file.split(os.path.sep)[-1].split('_'))[-1].split('.tiff'))[0] in frame_num_list]
+    else:
+        image_files = image_files_all[::int(np.ceil(len(image_files_all) / 10))] # only take 10 images
+        
+    # Add two reference images
+    overlay_file = os.path.join(cf.input_path, '1000.tiff')
+    no_overlay_file = os.path.join(cf.input_path, '1001.tiff')
+    
+    frame_nums = mp.Manager().list()
+    maps_xy_dct = mp.Manager().dict()
+    maps_dxdy_dct = mp.Manager().dict()
+    df_lst = mp.Manager().list()
+    df_frame_lst = mp.Manager().list()
+    
+    queue = mp.Manager().Queue()
+    listener = mp.Process(target=logger_process, args=(queue, setup_logger, log_file))
+    listener.start()
+    
+    pool = mp.Pool(processes=mp.cpu_count())
+
+    start = time.perf_counter()
+    pipeline_partial = partial(pipeline, queue, df_lst, df_frame_lst, frame_nums, maps_xy_dct, maps_dxdy_dct, cf.output_path, params)
+    pool.map(pipeline_partial, image_files)
+    print(f'Blob Detection time: {round(time.perf_counter() - start, 2)}')
+    df = pd.concat(df_lst, ignore_index=True)
+    df_frame = pd.concat(df_frame_lst, ignore_index=True)
+    print(frame_nums)
+    
+    queue.put(None)   
+    listener.join()
+
+    df_frame.sort_values(by='frame_num', key=lambda x: x.astype('int'), inplace=True, ignore_index=True)
+    df_frame['index'] = np.arange(len(df_frame.index))
+    
+    maps_xy_sorted = sorted(maps_xy_dct.items(), key=lambda x: int(x[0]))
+    maps_xy = [x[1] for x in maps_xy_sorted]
+    maps_dxdy_sorted = sorted(maps_dxdy_dct.items(), key=lambda x: int(x[0]))
+    maps_dxdy = [x[1] for x in maps_dxdy_sorted]
+    
+    # New Procedure
+    logger = logging.getLogger(__name__)
+    logger.info('Overlay Frame : %s Processing started', params['driver'])
+    
+    # Find FOV dot in overlay_image
+    overlay_image = cv2.imread(overlay_file)
+    if params['driver'] == 'MODEL':
+        pass
+    elif params['driver'] == 'FATP': # rotate 180 deg as FATP is mounted upside down
+        overlay_image = np.rot90(overlay_image, 2)  
+    
+    overlay_height, overlay_width, _ = overlay_image.shape
+    
+    fov_dot_overlay = blobs.find_fov(overlay_image, params, logger, '1000', overlay_height, overlay_width)
+    logger.info('Overlay Frame : FOV dot was found at %s', fov_dot_overlay.__str__())
+    
+    # Find dots in no_overlay_image
+    no_overlay_image = cv2.imread(no_overlay_file)
+    if params['driver'] == 'MODEL':
+        center_y_shift = params['model_center_y_shift']
+    elif params['driver'] == 'FATP': # rotate 180 deg as FATP is mounted upside down
+        center_y_shift = params['fatp_center_y_shift']
+        no_overlay_image = np.rot90(no_overlay_image, 2)  
+        
+    no_overlay_height, no_overlay_width, _ = no_overlay_image.shape
+        
+    no_overlay_frame = blobs.find_dots(no_overlay_image, params)
+    logger.info('Frame %s : Finding dots is complete, found %d dots', '1001', len(no_overlay_frame.dots))
+    
+    no_overlay_frame.center_dot = blobs.find_center_dot(no_overlay_frame.dots, no_overlay_height, no_overlay_width)
+    logger.info('Frame %s : Center dot was found at %s', '1001', no_overlay_frame.center_dot.__str__())
+    
+    no_overlay_med_size, no_overlay_med_dist = no_overlay_frame.calc_dot_size_dist()
+    logger.info('Frame %s Dot Size: %0.2f Distance: %0.2f', '1001', no_overlay_med_size, no_overlay_med_dist)
+    
+    logger.info('Starting slope calculations for frame %s', '1001')
+    no_overlay_proc = blobs.prep_image(no_overlay_image, params, normalize_and_filter=True, binarize=False)
+    no_overlay_init_hor_slope, no_overlay_init_ver_slope, no_overlay_hor_dist_error, no_overlay_ver_dist_error = blobs.get_initial_slopes(no_overlay_proc, no_overlay_height, no_overlay_width, ratio=0.3)
+    no_overlay_hor_slope, no_overlay_ver_slope = no_overlay_frame.get_slopes(no_overlay_init_hor_slope, no_overlay_init_ver_slope, no_overlay_hor_dist_error, no_overlay_ver_dist_error)
+    logger.info('Frame %s HSlope: %0.2f VSlope: %0.2f', '1001', no_overlay_hor_slope, no_overlay_ver_slope)
+    
+    no_overlay_hor_lines, no_overlay_ver_lines = no_overlay_frame.group_lines()
+    no_overlay_frame.find_index(logger, '1001', center_y_shift=center_y_shift)
+    logger.info('Finished indexing calculations for frame %s', '1001')
+    
+    no_overlay_frame.generate_map_xy(logger, '1001')
+    no_overlay_frame.generate_map_dxdy(params['dxdy_spacing'])  #4x spacing
+        
+    kpi.find_outliers(df_frame, width=4024, height=3036)
+    summary_df = pd.DataFrame({'frame_num': '1000-1001',
+                                  'total_dots' : len(no_overlay_frame.dots),
+                                  'center_dot_x' : no_overlay_frame.center_dot.x, 'center_dot_y' : no_overlay_frame.center_dot.y,
+                                  'fov_dot_x' : fov_dot_overlay.x, 'fov_dot_y' : fov_dot_overlay.y,
+                                  'median_dot_size' : no_overlay_med_size, 'median_dot_spacing' : no_overlay_med_dist,
+                                  'hor_slope' : no_overlay_hor_slope, 'ver_slope' : no_overlay_ver_slope,
+                                  'dxdy_spacing' : params['dxdy_spacing']}, index=[0])
+    summary = kpi.eval_KPIs(df_frame, params, summary_df, frame_nums, maps_xy, maps_dxdy, no_overlay_frame.map_dxdy)
+    
+    with open(csv_file_summary, 'w') as f:  # You will need 'wb' mode in Python 2.x
+        w = csv.DictWriter(f, summary.keys())
+        w.writeheader()
+        w.writerow(summary)      
+    df.to_csv(csv_file)
+    df_frame.to_csv(csv_file_frame)
+    
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+    
+
+    
+    
